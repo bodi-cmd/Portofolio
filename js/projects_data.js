19de1ecfeb0dac172efe178a57fae7d29e35e5b0
bodi-cmd <bogdan.barbu.dev@gmail.com>
2021-08-31 02:47:31 +0300

    object generated projects

--- js/projects_data.js
+++ js/projects_data.js
@@ -1,9 +1,36 @@
-var data =[
+var projects_obj = [
     {   
-        id:"chess",
-        animation:{
-            url:"../img/sah/sah",
-            extension:".webp"
-        }
+        id:"auto_chess",
+        title:"AutoChess",
+        summary:'Copii fiind, cu toții ne-am imaginat măcar o dată cum ar fi daca obiectele din jurul nostru ar prinde viață. Ei bine, fix de la aceleași idei a pornit si proiectul AutoChess. Acesta reprezinta o tabla de șah "în carne si oase", robotizată, capabilă sa țină locul unui adversar.',
+        link:'/',
+        image:'./img/sah/sah0000.webp',
+        tags:['C','Arduino','Python','3D Printing','Embedded']
+    },
+    {   
+        id:"the_hunter_master",
+        title:"The Hunter Master!",
+        summary:'<i>The Hunter Master!</i> este un joc destinat telefoanelor cu Android. Scopul jocului este acela de a vâna eficient cât mai multe animale, astfel colectând bani îmbunătățind cât mai multe bonusuri.',
+        link:'/',
+        image:'./img/renders/thehuntermaster.webp',
+        tags:['C#','Unity','MagicaVoxel']
+    },
+    {   
+        id:"chain_up",
+        title:"Chain Up!",
+        summary:'Stivuiește cât mai multe cutii fără să le scapi. Pare simplu? Doar încearcă! <i>Chain Up!</i> este un joc realizat pentru platforma Android, ce plănuiește sa îți puna reflexele la încercare.',
+        link:'/',
+        image:'./img/renders/chainup.webp',
+        tags:['C#','Unity']
+    },
+    {   
+        id:"wall_breaker",
+        title:"Wall Breaker",
+        summary:'Îți plac jocurile de tip infinite runner? Atunci <i>Wall Breaker</i> este exact ce căutai. Sparge unul dintre cele patru blocuri ale zidurilor pentru a face loc cometei să treacă. Adună puncte si deblochează toate culorile disponibile.',
+        link:'/',
+        image:'./img/renders/wallbreaker.webp',
+        tags:['C#','Unity']
     }
+
+
 ];
---
+++ js/projects_loader.js
@@ -0,0 +1,53 @@
+
+jQuery(document).ready(function(){
+    render_projects(projects_obj);
+
+    // $(document).on('click', '.tag', function(){
+    //     var filtered_data = [];
+    //     for(i in data){
+    //         var contains = false;
+    //         for(j in data[i].tags){
+    //             if(data[i].tags[j] == $(this).text()){
+    //                 contains = true;
+    //             }
+    //         }
+    //         if(contains)filtered_data.push(data[i]);
+    //     }
+    //     clear_projects();
+    //     render_projects(filtered_data);
+    //     $('html, body').animate({
+    //         scrollTop: $("#"+filtered_data[0].id+"_page").offset().top
+    //     }, 1000);
+    // });
+});
+
+function clear_projects(){
+    $("#projects").empty();
+    $("#spacer").remove();
+}
+
+function render_projects(data){
+    for(var i=0;i<data.length;i++){
+
+        $(".projects").append(`<div class="project flex">
+                                <div class="fl1">
+                                <img src="${data[i].image}" alt="" class="project_img">
+                                </div>
+                                <div class="fl1">
+                                <div class="content m-center">
+                                    <h1>${data[i].title}</h1>
+                                    <p>${data[i].summary}  <a href="${data[i].link}" class="project_link">Read more...</a></p>
+                                
+                                    <br style="clear:both" />
+                                    <div id="tags_${data[i].id}" class="tags">
+                                    </div>
+                                    <br style="clear:both" />
+                                </div> 
+                                </div>
+                            </div>`);
+    for(var j=0;j<data[i].tags.length;j++){
+        $("#tags_"+data[i].id).append(`<div class="tag"><p class="technology_tag">${data[i].tags[j]}</p></div>`);
+    }
+    }
+
+}
