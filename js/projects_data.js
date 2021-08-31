a2218563897d2f5bc745dd5414fcfa5afb5730aa
bodi-cmd <bogdan.barbu.dev@gmail.com>
2021-08-31 17:34:37 +0300

    proiecte noi

--- js/projects_data.js
+++ js/projects_data.js
@@ -1,5 +1,13 @@
 var projects_obj = [
     {   
+        id:"srtify",
+        title:".srtify",
+        summary:'Cu toții am apelat măcar odată la metode mai puțin etice de vizionare a filmelor. Dar ce te faci cu subtitrarea care acum e plină de caractere corupte în loc de diacritice? Well, acesta este prețul pirateriei...<b> Sau poți descărca această aplicație care îți repară toate subtitrările!</b>',
+        link:'/',
+        image:'./img/renders/laptop_srtify.webp',
+        tags:['C']
+    },
+    {   
         id:"auto_chess",
         title:"AutoChess",
         summary:'Copii fiind, cu toții ne-am imaginat măcar o dată cum ar fi daca obiectele din jurul nostru ar prinde viață. Ei bine, fix de la aceleași idei a pornit si proiectul AutoChess. Acesta reprezinta o tabla de șah "în carne si oase", robotizată, capabilă sa țină locul unui adversar.',
@@ -8,12 +16,28 @@ var projects_obj = [
         tags:['C','Arduino','Python','3D Printing','Embedded']
     },
     {   
+        id:"AES",
+        title:"AES Encryption",
+        summary:'<i>Advanced Encryption Standard</i> reprezinta standardul de cryptare la nivel mondial. Algoritmul grupeaza datele in blocuri de 128 de biți, apoi aplica operații matematice pe inelul GF(2^8), precum interschimbarea valorilor, permutarea liniilor, înmulțirea cu matrici, dar si operația logica sau exclusiv (<i> XOR </i>).',
+        link:'/',
+        image:'./img/renders/laptop_aes.webp',
+        tags:['C']
+    },
+    {   
         id:"the_hunter_master",
         title:"The Hunter Master!",
         summary:'<i>The Hunter Master!</i> este un joc destinat telefoanelor cu Android. Scopul jocului este acela de a vâna eficient cât mai multe animale, astfel colectând bani îmbunătățind cât mai multe bonusuri.',
         link:'/',
         image:'./img/renders/thehuntermaster.webp',
         tags:['C#','Unity','MagicaVoxel']
+    },
+    {   
+        id:"medapp",
+        title:"MedApp",
+        summary:'Majoritatea oamenilor sufera de afecțiuni care îi supun la anumite tratamente. Dar ce te faci în cazul în care ai nevoie de un medicament de la farmacie, iar tu nu te poți deplasa deoarece te aflii în carantina la domiciuliu. Proiectul <i>MedApp</i> are ca scop realizarea conexiunii dintre pacienți medici si curieri. Îți descrii simptomele online, unui medic, apoi primești rețeta pe care o poți comanda la tine acasă direct din aplicație.',
+        link:'/',
+        image:'./img/renders/laptop_medapp.webp',
+        tags:['HTML','CSS','Javascript','NodeJS','SQL','Bcrypt','PassportAuth','Express','ejs']
     },
     {   
         id:"chain_up",
