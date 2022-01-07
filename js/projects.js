0bb80bc25a566a05f510561bdf42294d72cef136
Bogdan Barbu <bogdan.barbu.dev@gmail.com>
2022-01-07 23:58:02 +0200

    fixed links

---
+++ js/projects.js
@@ -0,0 +1,7 @@
+$( ".mini" ).click(function() {
+    //window.open('/projects/' + this.id , '_self');
+    window.open(this.id);
+});
+$( ".minr" ).click(function() {
+    window.location.href = 'https://bogdanbarbu.com/projects/' + this.id;
+});
